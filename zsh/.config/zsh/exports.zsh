e1a1af13be0ab605149828073bbd7af417e2c160
Pedro Morim <pmorim@pm.me>
2023-01-25 09:48:42 +0000

    Add nvm support to zsh

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -17,6 +17,11 @@ export BROWSER="brave"
 export MANPAGER='nvim +Man!'
 export MANWIDTH=999
 
+# NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # Zoxide: a better `cd`
 eval "$(zoxide init zsh)"
 
