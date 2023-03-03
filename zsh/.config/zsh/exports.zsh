25a3b802ab47ffe49e98f6cd65fa1485547e06f2
pmorim <pmorim@pm.me>
2023-03-03 23:46:46 +0000

    Remove useless zsh configs

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -17,16 +17,5 @@ export PATH="$HOME/.local/bin":$PATH
 export PATH="$HOME/.cargo/bin":$PATH
 export PATH="$HOME/.local/share/neovim/bin":$PATH
 
-# SSH
-export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
-
-# Node Version Manager
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# Zoxide: a better `cd`
-eval "$(zoxide init zsh)"
-
 # Starship prompt
 eval "$(starship init zsh)"
