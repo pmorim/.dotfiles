5a6f0ff19cc719ebc12d084e1a3a4d3312ab227a
pmorim <pmorim@pm.me>
2023-10-17 21:46:57 +0100

    Add lang exports

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -5,21 +5,34 @@ HISTFILE=~/.zsh_history
 HISTSIZE=1000000
 SAVEHIST=1000000
 
-# General
+# General Vars
 export EDITOR="nvim"
 export TERMINAL="alacritty"
 export BROWSER="brave"
 export MANPAGER="nvim +Man!"
 export MANWIDTH=999
 
-# Volta (Node.js version manager)
-export VOLTA_HOME="$HOME/.volta"
-
-# Path
+# General Paths
 export PATH="$HOME/.local/bin":$PATH
-export PATH="$HOME/.cargo/bin":$PATH
 export PATH="$HOME/.local/share/neovim/bin":$PATH
+
+# Volta
+export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
+
+# Bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+[ -s "/home/pmorim/.bun/_bun" ] && source "/home/pmorim/.bun/_bun"
+
+# Rust
+export PATH="$HOME/.cargo/bin":$PATH
+
+# Odin
+export PATH="$HOME/.odin/bin":$PATH
+
+# OCaml
+[[ ! -r /home/pmorim/.opam/opam-init/init.zsh ]] || source /home/pmorim/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
 
 # Starship prompt
 eval "$(starship init zsh)"
